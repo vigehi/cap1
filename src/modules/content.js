0f27c98d855ad4fe843f107f3eeda855ce82e4f1
Dennis <vigehi2017@gmail.com>
2022-09-10 00:32:43 +0300

    Fix numbers

--- src/modules/content.js
+++ src/modules/content.js
@@ -3,7 +3,7 @@ import episodelikes from './episodelikes.js';
 
 const commpopup = document.getElementById('popup');
 let count = 0;
-
+const counter = document.getElementById('count');
 const episodecards = document.getElementById('container');
 let currentValue = 0;
 const appId = 'KM960h36MEAZF5DO9Wvb';
@@ -126,7 +126,6 @@ const showcommpopup = async () => {
 
 // Render all episodes card
 const episodestats = async () => {
-  const counter = document.getElementById('count');
   const episodeArray = await fetchAPIdata();
   episodeArray.slice(0, 10).forEach((episode) => {
     episodelikes().then((res) => {
