fe8f3c558598566e5993da3873bf9380665b60af
Dennis <vigehi2017@gmail.com>
2022-09-11 01:04:10 +0300

    updates

---
+++ src/modules/__test__/pageCounter.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+ import itemCounter from '../counter.js';
+ import { getepisodeData, getComment } from './mocks/api.js';
+ 
+ // test count item list
+ describe('Count the total a recipes on page from api', () => {
+   test('Must return total 3 items', () => {
+     // Arrange
+     document.body.innerHTML = '<div id="list">'
+       + '</div>';
+ 
+     // Act
+     const result = itemCounter(document.getElementById('list'), getepisodeData());
+ 
+     // Assert
+     expect(result).toBe(3);
+   });
+ });
+ 
+ // Test of comment list
+ 
+ describe('Count the total comment on page from api', () => {
+   test('Must return total 6 items', () => {
+     // Arrange
+     document.body.innerHTML = '<div id="list">'
+      + '</div>';
+     // Act
+     const result = itemCounter(document.getElementById('list'), getComment());
+     // Assert
+     expect(result).toBe(6);
+   });
+ });
